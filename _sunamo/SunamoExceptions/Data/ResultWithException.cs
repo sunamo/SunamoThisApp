3d472ec6ccba68ee3099f5faf16a5ccea2dea609
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-05-21 20:36:35 +0200

    Everyone on nuget

---
+++ _sunamo/SunamoExceptions/Data/ResultWithException.cs
@@ -0,0 +1,30 @@
+namespace SunamoThisApp;
+
+
+public class ResultWithException<T>
+{
+    public T Data { get; set; }
+    /// <summary>
+    ///     only string, because Message property isn't editable after instatiate
+    ///     Usage: FubuCsprojFile
+    /// </summary>
+    public string exc { get; set; }
+    public ResultWithException(T data)
+    {
+        Data = data;
+    }
+    public ResultWithException(string exc)
+    {
+        this.exc = exc;
+    }
+    public ResultWithException(Exception exc)
+    {
+        this.exc = Exceptions.TextOfExceptions(exc);
+    }
+    /// <summary>
+    /// Pro případ že data josu string což je typ i exception
+    /// </summary>
+    public ResultWithException()
+    {
+    }
+}
