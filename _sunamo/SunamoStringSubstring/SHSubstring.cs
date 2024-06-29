ceb9e6da5850f91c6346cc7dae26a4c06c78401e
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-29 08:57:46 +0200

    moved all in _sunamo to right subfolders by original project

---
+++ _sunamo/SunamoStringSubstring/SHSubstring.cs
@@ -0,0 +1,8 @@
+namespace SunamoThisApp;
+internal class SHSubstring
+{
+    internal static string SubstringIfAvailable(string input, int lenght)
+    {
+        return input.Length > lenght ? input.Substring(0, lenght) : input;
+    }
+}
