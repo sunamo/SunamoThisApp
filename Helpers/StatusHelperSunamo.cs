ed5053c135dcfae4b24b7fb8510d5c4e80cc4252
sunamo.cz_asus <radek.jancik@sunamo.cz>
2024-06-30 15:42:51 +0200

    All buildable, merged partial classes

--- Helpers/StatusHelperSunamo.cs
+++ Helpers/StatusHelperSunamo.cs
@@ -1,8 +1,47 @@
 namespace SunamoThisApp;
 
-
-public partial class StatusHelperSunamo
+public class StatusHelperSunamo
 {
+    public static TypeOfMessageTA IsStatusMessage(string resp)
+    {
+        var r = resp;
+        return IsStatusMessage(ref r);
+    }
+
+    /// <summary>
+    /// If dont start with none, return Ordinal
+    /// </summary>
+    /// <param name = "resp"></param>
+    public static TypeOfMessageTA IsStatusMessage(ref string resp)
+    {
+        if (SHTrim.TrimIfStartsWith(ref resp, error))
+        {
+            return TypeOfMessageTA.Error;
+        }
+        else if (SHTrim.TrimIfStartsWith(ref resp, warning))
+        {
+            return TypeOfMessageTA.Warning;
+        }
+        else if (SHTrim.TrimIfStartsWith(ref resp, success))
+        {
+            return TypeOfMessageTA.Success;
+        }
+        else if (SHTrim.TrimIfStartsWith(ref resp, info))
+        {
+            return TypeOfMessageTA.Information;
+        }
+        else if (SHTrim.TrimIfStartsWith(ref resp, information))
+        {
+            return TypeOfMessageTA.Information;
+        }
+        else if (SHTrim.TrimIfStartsWith(ref resp, appeal))
+        {
+            return TypeOfMessageTA.Appeal;
+        }
+
+        return TypeOfMessageTA.Ordinal;
+    }
+
     public const string error = "error:";
     public const string warning = "warning:";
     public const string success = "success:";
