ac20d18517f169fa73179c87d8cc62fa38645a10
sunamo.cz <radek.jancik@sunamo.cz>
2024-07-16 15:35:26 +0200

    buildable, before standardize

--- _sunamo/SunamoExceptions/_AddedToAllCsproj/FSSunamoExceptions.cs
+++ _sunamo/SunamoExceptions/_AddedToAllCsproj/FSSunamoExceptions.cs
@@ -1,5 +1,7 @@
-namespace SunamoThisApp._sunamo.SunamoExceptions._AddedToAllCsproj;
-internal class FSSunamoExceptions
+//namespace SunamoThisApp._sunamo.SunamoExceptions._AddedToAllCsproj;
+
+//namespace SunamoThisApp._sunamo.SunamoExceptions._AddedToAllCsproj;
+internal class FS
 {
     internal static void CreateUpfoldersPsysicallyUnlessThere(string nad)
     {
